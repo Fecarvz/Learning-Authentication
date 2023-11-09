107f9a9175102e5cd66f59112d1b7fbc1b8abdc4
Fecarvz <felipe.carvalho1703@usp.br>
2023-11-09 19:59:27 -0300

    level 4

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption")
-const md5 = require("md5")
+// const md5 = require("md5")
+const bcrypt = require("bcryptjs")
+const saltRounds = 10;
 
 const app = express();
 
@@ -35,36 +37,45 @@ app.get("/register", (req, res) => {
     res.render("register");
 })
 
+
 app.post("/register", async (req, res) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
         const newUser = new User({
             email: req.body.username,
-            password: md5(req.body.password)
-        })
+            password: hashedPassword
+        });
         await newUser.save();
-    } catch(err) {
-        res.status(500).send({message: "Erro ao registrar a conta, tente novamente"});
-    } finally {
         res.status(201).render("secrets");
+    } catch (err) {
+        res.status(500).send({ message: "Erro ao registrar a conta, tente novamente" });
     }
-})
+});
+
+
+
+
 
 app.post("/login", async (req, res) => {
     try {
         const username = req.body.username;
         const password = req.body.password;
 
-        const user = await User.findOne({email: username});
-        if(user){
-            if(user.password === md5(password))
+        const user = await User.findOne({ email: username });
+        if (user) {
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (isPasswordValid) {
                 res.status(200).render("secrets");
+            } else {
+                res.status(200).render("login", { message: "Credenciais inválidas. Tente novamente." });
+            }
         } else {
-            res.status(200).render("login");
+            res.status(200).render("login", { message: "Usuário não encontrado. Por favor, registre-se." });
         }
-    } catch(err) {
-        res.status(500).render("login", {message: "Erro interno, tente novamente mais tarde."});
+    } catch (err) {
+        res.status(500).render("login", { message: "Erro interno, tente novamente mais tarde." });
     }
-})
+});
 
 
 app.listen(3000, () => {
